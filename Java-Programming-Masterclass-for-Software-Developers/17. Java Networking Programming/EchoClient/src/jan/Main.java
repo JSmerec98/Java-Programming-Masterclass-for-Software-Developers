d4b1f5e748a3f12f8fe31e3d295fc27d12504467
Jan Smerecki <57664542+JSmerec98@users.noreply.github.com>
2020-05-26 12:02:37 +0200

    Java Networking Programming

--- Java-Programming-Masterclass-for-Software-Developers/17. Java Networking Programming/EchoClient/src/jan/Main.java
+++ Java-Programming-Masterclass-for-Software-Developers/17. Java Networking Programming/EchoClient/src/jan/Main.java
@@ -5,12 +5,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         try(Socket socket = new Socket("localhost",5000)) {
+
+            socket.setSoTimeout(5000);
             BufferedReader echoes = new BufferedReader(
                     new InputStreamReader(socket.getInputStream()));
             PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream(), true);
@@ -25,12 +29,13 @@ public class Main {
 
                 stringToEcho.println(echoString);
 
-                if(!echoString.equals("exit")) {
+                if (!echoString.equals("exit")) {
                     response = echoes.readLine();
                     System.out.println(response);
                 }
             } while (!echoString.equals("exit"));
-
+        } catch (SocketTimeoutException e) {
+            System.out.println("The socker timed out!");
         } catch (IOException e) {
             System.out.println("Client Error: " + e.getMessage());
         }
--- Java-Programming-Masterclass-for-Software-Developers/17. Java Networking Programming/EchoServer/src/jan/Main.java
+++ Java-Programming-Masterclass-for-Software-Developers/17. Java Networking Programming/EchoServer/src/jan/Main.java
@@ -1,9 +1,6 @@
 package jan;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,19 +8,8 @@ public class Main {
 
     public static void main(String[] args) {
         try(ServerSocket serverSocket = new ServerSocket(5000)) {
-            Socket socket = serverSocket.accept();
-            System.out.println("Client Connected");
-            BufferedReader input = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
             while (true) {
-                String echoString = input.readLine();
-                if(echoString.equals("exit")) {
-                    break;
-                }
-
-                output.println("Echo from server: " + echoString) ;
+                new Echoer(serverSocket.accept()).start();
             }
         } catch (IOException e) {
             System.out.println("Server exception: " + e.getMessage());
