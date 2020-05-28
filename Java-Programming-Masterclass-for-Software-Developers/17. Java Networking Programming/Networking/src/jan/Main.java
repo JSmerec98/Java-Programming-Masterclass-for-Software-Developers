96d0f00fb5ed3fefbe19aa769586cd5d4216834d
Jan Smerecki <57664542+JSmerec98@users.noreply.github.com>
2020-05-28 12:32:21 +0200

    Java Networking Programming

--- Java-Programming-Masterclass-for-Software-Developers/17. Java Networking Programming/Networking/src/jan/Main.java
+++ Java-Programming-Masterclass-for-Software-Developers/17. Java Networking Programming/Networking/src/jan/Main.java
@@ -1,37 +1,68 @@
 package jan;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-//            URI uri = new URI("http://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
-            URI baseURI = new URI("http://username:password@myserver.com:5000/");
-            URI uri = new URI("/catalogue/phones?os=android#samsung");
-            URI resolvedUri = baseURI.resolve(uri);
+            URL url = new URL("http://example.org");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent","Chrome");
+            connection.setReadTimeout(30000);
 
-            URL url = resolvedUri.toURL();
-            System.out.println("URL = " + url);
-//            URI uri = new URI("hello");
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response code: " + responseCode);
 
-//            System.out.println("Scheme = " + uri.getScheme());
-//            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
-//            System.out.println("Authority = " + uri.getAuthority());
-//            System.out.println("User info = " + uri.getUserInfo());
-//            System.out.println("Host = " + uri.getHost());
-//            System.out.println("Port = " + uri.getPort());
-//            System.out.println("Path = " + uri.getPath());
-//            System.out.println("Query = " + uri.getQuery());
-//            System.out.println("Fragment = " + uri.getFragment());
+            if(responseCode != 200) {
+                System.out.println("Error reading web page");
+                return;
+            }
 
-        } catch (URISyntaxException e) {
-            System.out.println("URI Bad Syntax: " + e.getMessage());
+            BufferedReader inputReader = new BufferedReader(
+            new InputStreamReader(connection.getInputStream()));
+
+            String line;
+
+            while((line = inputReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            inputReader.close();
+
+
+//            urlConnection.setDoOutput(true);
+//            urlConnection.connect();
+//
+//            BufferedReader inputStream = new BufferedReader(
+//                    new InputStreamReader(urlConnection.getInputStream()));
+//
+//            Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
+//            for(Map.Entry<String, List<String>> entry: headerFields.entrySet()) {
+//                String key = entry.getKey();
+//                List<String> value = entry.getValue();
+//                System.out.println("-----key = " + key);
+//                for (String string: value) {
+//                    System.out.println("value = " + value);
+//                }
+//            }
+
+//            String line = "";
+//            while(line != null) {
+//                line = inputStream.readLine();
+//                System.out.println(line);
+//            }
+//            inputStream.close();
+
         } catch (MalformedURLException e) {
-            System.out.println("URL Malformed: " + e.getMessage());
+            System.out.println("Malformed URL: " + e.getMessage());
+        }catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
         }
     }
 }
